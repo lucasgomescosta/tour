c7bebe89a60f800bcb297f181d9bb42b01429a18
Lucas Gomes <lucasgomesc582@gmail.com>
2021-09-01 12:11:29 -0400

    final curso

---
+++ src/main/kotlin/com/acmne/tour/config/CacheConfig.kt
@@ -0,0 +1,14 @@
+package com.acmne.tour.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+    @Bean
+    fun cacheManger(): CacheManager = ConcurrentMapCacheManager("promocoes")
+}
--- src/main/kotlin/com/acmne/tour/service/impl/PromocaoServiceImpl.kt
+++ src/main/kotlin/com/acmne/tour/service/impl/PromocaoServiceImpl.kt
@@ -5,6 +5,8 @@ import com.acmne.tour.model.Promocao
 import com.acmne.tour.repository.PromocaoRepository
 import com.acmne.tour.service.PromocaoService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.context.annotation.Bean
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
@@ -19,14 +21,17 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoS
         return promocaoRepository.findById(id).orElseGet(null)
     }
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun create(promocao: Promocao) {
         promocaoRepository.save(promocao)
     }
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun delete(id: Long) {
         promocaoRepository.deleteById(id)
     }
 
+    @CacheEvict("promocoes", allEntries = true)
     override fun update(id: Long, promocao: Promocao) {
         create(promocao)
     }
@@ -34,6 +39,7 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoS
     override fun searchByLocal(localFilter: String): List<Promocao> =
        listOf()
 
+    @Cacheable("promocoes")
     override fun getAll(start: Int, size: Int): List<Promocao> {
         val pages: Pageable = PageRequest.of(start, size, Sort.by("local").ascending())
         return promocaoRepository.findAll(pages).toList()
@@ -46,7 +52,7 @@ class PromocaoServiceImpl(val promocaoRepository: PromocaoRepository): PromocaoS
         this.promocaoRepository.findAll(Sort.by("local").descending()).toList()
 
     override fun getAllByPrecoMenorQuer9000(): List<Promocao> {
-        return this.promocaoRepository.findByPrecoMenorQue()
+        return this.promocaoRepository.findByPrecoMenorQue(9000.0, 6)
     }
 
 
